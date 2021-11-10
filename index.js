6658c4f251653f5a963904807e1a20793cc2878c
Abdulazizkhan001 <abdualzizbotirxonov93@gmail.com>
2021-11-10 13:05:42 +0500

    object

--- index.js
+++ index.js
@@ -1,65 +1,69 @@
-// // Homework-1
-// var fruits = ['apple', 12, 'Orange', 'Banana', 'Kiwi'];
-// var counta = 0;
-// for (var i = 0; i < fruits.length; i++) {
-//     for (j = 0; j <= fruits.length; j++) {
-//         fruits[i][j] === 'a' && counta++;
+var user = {
+    id: 1,
+    year: 2010,
+    name: 'Otabek',
+    age: 21,
+    key: 'test'
+}
+
+var NAMDU = [{
+    id: 1,
+    year: 2010,
+    name: 'Otabek',
+    age: 21,
+    key: 'test'
+},
+{
+    id: 2,
+    year: 2011,
+    name: 'Iqbol',
+    age: 21,
+    key: 'test'
+},
+{
+    id: 3,
+    year: 2012,
+    name: 'Zokir',
+    age: 21,
+    key: 'test'
+}
+];
+
+key = 'name';
+//     By id
+// const onDelete = (id) => {
+//     for (let i = 0; i < NAMDU.length; i++) {
+//         if (id === NAMDU[i].id) {
+//             return NAMDU[i];
+//         }
 //     }
 // }
-// console.log(counta)
+// console.log(onDelete(3))
 
-// // Homework-2
-// var number = [10, 5, 11, 'apple', 2, 3, 4, 5, 6];
-// let sum = 0;
-// number.filter((value) => typeof value === 'number').map((value, index) => {
-//     sum += value;
-// })
-// console.log(sum);
-// // Homework-3.1
-// var bigNum = [9, 45, 7, 18, 6, 2, 17, 8, 40, 'apple'];
-// var copyBigNum = bigNum.sort((a, b) => a - b).filter((value) => typeof value === 'number');
-// for (var i = 0; i < copyBigNum.length; i++) {
-//     var maxNum = 0;
-//     maxNum = copyBigNum[i - 1]
+//     By name
+const onDelete = (name) => {
+    for (let i = 0; i < NAMDU.length; i++) {
+        if (name === NAMDU[i].name) {
+            return NAMDU[i];
+        }
+    }
+}
+console.log(onDelete('Zokir'))
+
+
+
+// //        By year
+// const onDelete = (year) =>{
+
 // }
-// console.log(maxNum);
-// // Homework-3.2
-
-// for (var i = copyBigNum.length; i >= 0; i--) {
-//     var minNum = 0;
-//     minNum = copyBigNum[i + 1]
-// }
-// console.log(minNum);
-// // Homework-4
-// var toqson = 0;
-// var juftson = 0;
-// number.filter((value) => typeof value === 'number').map((value, index) => {
-//     index % 2 === 0 ? juftson += value : toqson += value;
-// })
-// console.log('Juft son - ' + `${juftson}`, "  ", 'Toq son - ' + `${toqson}`);
+// console.log(onDelete(2012))
 
 
-let delNum = [12, 34, 54, 19, 2, 66, 44, 22];
 
 
-// delNum.map((value)=>{
-//     return newAr.unshift(value)
-// })
-// console.log(delNum);
 
-// let newAr = [];
-// for (let i = delNum.length - 1; i >= 0 ; i--) {
-//         newAr.push(delNum[i])
-    
-//     }
 
-// for( let i = 0; i <= delNum.length-1; i++)
-// {
-//     newAr.unshift(delNum[i])
-// }
-// console.log(newAr)
 
-// var nm = [1,5,8,8,9,9]
 
-// nm.fill(0,0,4)
-// console.log(nm)
+
+
